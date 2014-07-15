bdada8c083cac0878abd9b246fd0bc350f70a9c0
Kai Ellinger <coding@blicke.de>
2014-07-15 16:01:53 +0200

    updated create docs file

--- createDocs.bash
+++ createDocs.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
-pod2man grepWlsLogFile.pl  >grepWlsLogFile.1.man
+pod2man grepWlsLogFile.pl  >docs/man1/grepWlsLogFile.1
+pod2html --title="grepWlsLogFile.pl - HELP" --infile=grepWlsLogFile.pl --outfile=docs/index.html
+rm pod2htmd.tmp
 pod2markdown grepWlsLogFile.pl README.md
